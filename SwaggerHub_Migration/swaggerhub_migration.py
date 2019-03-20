482f4e85c8231e963e64b505ff17368295a3bdf4
Steven Colon <steven.j.colon@gmail.com>
2019-03-20 13:05:07 -0400

    Added better error handling and support for any two orgs either in a single installation or multiple.

--- SwaggerHub_Migration/swaggerhub_migration.py
+++ SwaggerHub_Migration/swaggerhub_migration.py
@@ -5,62 +5,73 @@ Created on Mar 19, 2019
 '''
 import requests
 import json
+import helper_functions
 
+    
 with open('config.json', 'r') as file:
     config = json.load(file)
 
-saas_api_key= config['SAAS']['API_KEY']
-saas_registry_api = config['SAAS']['REGISTRY_API_BASEPATH']
-saas_org = config['SAAS']['ORG']
+export_org_api_key= config['EXPORTORG']['API_KEY']
+export_org_registry_api = config['EXPORTORG']['REGISTRY_API_BASEPATH']
+export_org_name = config['EXPORTORG']['ORG']
 
-onprem_api_key= config['ONPREM']['API_KEY']
-onprem_registry_api = config['ONPREM']['REGISTRY_API_BASEPATH']
-onprem_org = config['ONPREM']['ORG']
+import_org_api_key= config['IMPORTORG']['API_KEY']
+import_org_registry_api = config['IMPORTORG']['REGISTRY_API_BASEPATH']
+import_org_name = config['IMPORTORG']['ORG']
+
 
 #Pull specs in the outgoing org
-org_specs = requests.get(saas_registry_api + saas_org, headers= {'Authorization': saas_api_key, 'accept': 'application/json'}).json()
+org_specs_call = requests.get(export_org_registry_api + export_org_name, headers= {'Authorization': export_org_api_key})
+org_specs_json = org_specs_call.json()
 
-print("Migrating " + str(org_specs["totalCount"]) + " OAS Specs... \n")
+if len(org_specs_json['apis']) == 0:
+    raise RuntimeError("No APIs Found in SAAS Org")
+
+print("Migrating " + str(org_specs_json["totalCount"]) + " OAS Specs from " + export_org_registry_api + export_org_name + " to " + import_org_registry_api + import_org_name)
 
 #parse api spec metadata from org call
-for api_metadata in org_specs["apis"]:
-#    print(str(api_spec) + "\n\n\n\n\n\n")
+for api_metadata in org_specs_json["apis"]:
+    #Remove Default Version number from API Url so that we can pull all versions
     api_url = api_metadata["properties"][0]["url"] 
     last_slash = api_url.rindex('/', 0)
     formatted_api_url = api_url[0: last_slash]
-
+    formatted_api_url = helper_functions.verify_http_type(formatted_api_url, export_org_registry_api)
+    
+        
     api_sh_name = formatted_api_url[formatted_api_url.rindex('/', 0) + 1 : len(formatted_api_url)]
     print("Name - " + api_sh_name)
     print("Pulling versions from " + formatted_api_url)
 
-    #pull each version of the spec 
-    api_versions= requests.get(formatted_api_url, headers={'Authorization': saas_api_key, 'accept': 'application/json'}).json()
+    #Pull json that shows each version of the spec 
+    api_versions_call= requests.get(formatted_api_url, headers={'Authorization': export_org_api_key})
+    api_versions_json = api_versions_call.json()
 
-    print("Found " + str(api_versions["totalCount"]) + " versions ...")
+    print("Found " + str(api_versions_json["totalCount"]) + " versions ...")
+    
     #Pull API Version URLs
-    for api_version in api_versions['apis']:
-        api_version_url = api_version["properties"][0]["url"]
+    for api_version in api_versions_json['apis']:
+        api_version_url = helper_functions.verify_http_type(api_version["properties"][0]["url"], export_org_registry_api)
         print(api_version_url)
         #Get spec of single API Version
-        api_version_spec_call = requests.get(api_version_url, headers = {'Authorization': saas_api_key, 'accept': 'application/json'})
+        api_version_spec_call = requests.get(api_version_url, headers = {'Authorization': export_org_api_key})
         #print("Status of API Version Spec Call - " +  str(api_version_spec_call.status_code))
         
         #push spec to OnPrem 
-        onprem_post_url = onprem_registry_api + onprem_org + '/' + api_sh_name + '?isPrivate=true&force=true'
+        import_org_post_url = import_org_registry_api + import_org_name + '/' + api_sh_name + '?isPrivate=true&force=true'
         
-        #print("Posting Spec to - " + onprem_post_url)
+        print("Posting Spec to - " + import_org_post_url)
         
-        onprem_post_call = requests.post(onprem_post_url, headers={'Authorization':onprem_api_key}, json=api_version_spec_call.json())
+        onprem_post_call = requests.post(import_org_post_url, headers={'Authorization': import_org_api_key}, json=api_version_spec_call.json())
+        
         
         if(onprem_post_call.status_code != 201 and onprem_post_call.status_code != 200):
-            print('INVALID ONPREM POST RESPONSE')
-            exit()
+            raise RuntimeError("Invalid OnPrem API Response - " + onprem_post_call.text)
         else:
             print("API Version Uploaded to OnPrem")
             
     print("\n")
-
-
+    
+    
 
 
         
--- SwaggerHub_Migration/tests/swaggerhub_org_teardown.py
+++ SwaggerHub_Migration/tests/swaggerhub_org_teardown.py
@@ -8,36 +8,37 @@ Created on Mar 19, 2019
 #IMPORTANT NOTE - THIS SCRIPT WILL DELETE EVERY SPEC IN A ORG
 import requests
 import json
+import helper_functions
 
 input("WARNING - This Script will delete all API specs from a SwaggerHub Org. Press Enter to continue ...")
 with open('../config.json', 'r') as file:
     config = json.load(file)
     
-onprem_api_key= config['ONPREM']['API_KEY']
-onprem_registry_api = config['ONPREM']['REGISTRY_API_BASEPATH']
-onprem_org = config['ONPREM']['ORG']
+import_org_api_key= config['IMPORTORG']['API_KEY']
+import_org_registry_api = config['IMPORTORG']['REGISTRY_API_BASEPATH']
+import_org_name = config['IMPORTORG']['ORG']
 
 #Pull specs in the outgoing org
-org_specs_call = requests.get(onprem_registry_api + onprem_org, headers= {'Authorization': onprem_api_key, 'accept': 'application/json'})
-org_specs = org_specs_call.json()
+org_specs_call = requests.get(import_org_registry_api + import_org_name, headers= {'Authorization': import_org_api_key, 'accept': 'application/json'})
+org_specs_json = org_specs_call.json()
 
-if len(org_specs['apis']) == 0:
-    print("ERROR No APIs Found")
-    exit()
+if len(org_specs_json['apis']) == 0:
+    raise RuntimeError("No APIs Found in Org- " + import_org_registry_api + import_org_name)
 
 
-print("Tearing down the " + onprem_org + " from SwaggerHub OnPrem installation")
+print("Tearing down the " + import_org_name + " from " + import_org_registry_api)
 
 #parse api spec metadata from org call
-for api_metadata in org_specs["apis"]:
+for api_metadata in org_specs_json["apis"]:
     
     api_url = api_metadata["properties"][0]["url"] 
     last_slash = api_url.rindex('/', 0)
     formatted_api_url = api_url[0: last_slash]
-
+    formatted_api_url = helper_functions.verify_http_type(formatted_api_url, import_org_registry_api)
+    
     api_sh_name = formatted_api_url[formatted_api_url.rindex('/', 0) + 1 : len(formatted_api_url)]
     
-    api_delete_call= requests.delete(onprem_registry_api + onprem_org +"/"+ api_sh_name, headers={'Authorization': onprem_api_key, 'accept': 'application/json'})
+    api_delete_call= requests.delete(import_org_registry_api + import_org_name +"/"+ api_sh_name, headers={'Authorization': import_org_api_key, 'accept': 'application/json'})
     
     if api_delete_call.status_code == 200:
         print("Successfully Deleted " + api_sh_name)
@@ -45,4 +46,5 @@ for api_metadata in org_specs["apis"]:
         print("Error has occurred")
         print(api_delete_call.text)
 
+    
     
