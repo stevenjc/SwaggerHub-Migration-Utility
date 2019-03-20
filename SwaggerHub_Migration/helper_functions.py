7b369d6d8d4f2b31947dbef209ba17f9112cffdf
Steven Colon <steven.j.colon@gmail.com>
2019-03-20 15:30:38 -0400

    Added support for migrating domains, refactored into more modular method.

--- SwaggerHub_Migration/helper_functions.py
+++ SwaggerHub_Migration/helper_functions.py
@@ -3,8 +3,11 @@ Created on Mar 20, 2019
 
 @author: Steven.Colon
 '''
-
 #Method to verify two urls using either HTTP or HTTPS (set by the basepath)
 def verify_http_type(dynamic_url, basepath):
-    if(basepath.startswith("https") & dynamic_url.startswith("http")):
+    if basepath.startswith("https") and not dynamic_url.startswith("https") :
         return dynamic_url.replace('http', 'https')
+    else:
+        return dynamic_url
+    
+
--- SwaggerHub_Migration/tests/swaggerhub_org_teardown.py
+++ SwaggerHub_Migration/tests/swaggerhub_org_teardown.py
@@ -15,36 +15,47 @@ with open('../config.json', 'r') as file:
     config = json.load(file)
     
 import_org_api_key= config['IMPORTORG']['API_KEY']
-import_org_registry_api = config['IMPORTORG']['REGISTRY_API_BASEPATH']
+import_org_registry_basepath = config['IMPORTORG']['REGISTRY_API_BASEPATH']
 import_org_name = config['IMPORTORG']['ORG']
 
-#Pull specs in the outgoing org
-org_specs_call = requests.get(import_org_registry_api + import_org_name, headers= {'Authorization': import_org_api_key, 'accept': 'application/json'})
-org_specs_json = org_specs_call.json()
-
-if len(org_specs_json['apis']) == 0:
-    raise RuntimeError("No APIs Found in Org- " + import_org_registry_api + import_org_name)
-
-
-print("Tearing down the " + import_org_name + " from " + import_org_registry_api)
-
-#parse api spec metadata from org call
-for api_metadata in org_specs_json["apis"]:
+def main():
+    import_org_registry_api = import_org_registry_basepath + "apis/"
+    import_org_domains_url = import_org_registry_basepath + "domains/"
+    #Pull specs in the outgoing org
+    org_specs_call = requests.get(import_org_registry_api + import_org_name, headers= {'Authorization': import_org_api_key, 'accept': 'application/json'})
+    org_apis_json = org_specs_call.json()
     
-    api_url = api_metadata["properties"][0]["url"] 
-    last_slash = api_url.rindex('/', 0)
-    formatted_api_url = api_url[0: last_slash]
-    formatted_api_url = helper_functions.verify_http_type(formatted_api_url, import_org_registry_api)
+    if len(org_apis_json['apis']) == 0:
+        raise RuntimeError("No APIs Found in Org- " + import_org_registry_api + import_org_name)
     
-    api_sh_name = formatted_api_url[formatted_api_url.rindex('/', 0) + 1 : len(formatted_api_url)]
     
-    api_delete_call= requests.delete(import_org_registry_api + import_org_name +"/"+ api_sh_name, headers={'Authorization': import_org_api_key, 'accept': 'application/json'})
+    print("Tearing down the " + import_org_name + " from " + import_org_registry_api)
     
-    if api_delete_call.status_code == 200:
-        print("Successfully Deleted " + api_sh_name)
-    else:
-        print("Error has occurred")
-        print(api_delete_call.text)
-
+    parse_org(org_apis_json, import_org_registry_api)
+    
+    org_domains_call = requests.get(import_org_domains_url + import_org_name, headers= {'Authorization': import_org_api_key, 'accept': 'application/json'})
+    org_domains_json = org_domains_call.json()
+    
+    parse_org(org_domains_json, import_org_domains_url)
+    
+def parse_org(org_json, import_url):
+    #parse api spec metadata from org call
+    for metatdata in org_json["apis"]:
+        
+        url = metatdata["properties"][0]["url"] 
+        last_slash = url.rindex('/', 0)
+        formatted_url = url[0: last_slash]
+        formatted_url = helper_functions.verify_http_type(formatted_url, import_url)
+        
+        sh_name = formatted_url[formatted_url.rindex('/', 0) + 1 : len(formatted_url)]
+        
+        delete_call= requests.delete(import_url + import_org_name +"/"+ sh_name, headers={'Authorization': import_org_api_key, 'accept': 'application/json'})
+        
+        if delete_call.status_code == 200:
+            print("Successfully Deleted " + sh_name)
+        else:
+            print("Error has occurred")
+            print(delete_call.text)
+main()
     
     
